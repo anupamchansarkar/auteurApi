005a254008c7aa21f2a33fb40ccae8348b13c2b3
achansarkar <achansarkar@barracuda.com>
2019-02-11 11:02:23 -0800

    Added basic framework for website

--- auteur.py
+++ auteur.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@application.route("/home")
+def home():
+    return render_template('home.html')
+    
+@application.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
