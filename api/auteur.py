3e51067152896d0732fa741ed33f1a6bbf76e178
root <root@vm.auteur.com>
2019-02-23 22:04:46 -0800

    fixed issues

--- api/auteur.py
+++ api/auteur.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
 application = Flask(__name__)
 
 application.config['SECRET_KEY'] = '5f0c6f9efd1fe89814a4006f24e9f732'
 
 @application.route("/")
-@application.route("/register", methods=['POST'])
+@application.route("/register", methods=['GET','POST'])
 def register():
-	return
+	return "TEST"
 	
 @application.route("/login", methods=['POST'])
 def login():
