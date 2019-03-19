e9602a975286a3947ab61243d0325b0b33905066
achansarkar <achansarkar@barracuda.com>
2019-03-18 23:33:57 -0700

    removed all code that renders html

--- auteur.py
+++ auteur.py
@@ -5,32 +5,13 @@ application = Flask(__name__)
 application.config['SECRET_KEY'] = '5f0c6f9efd1fe89814a4006f24e9f732'
 
 @application.route("/")
-@application.route("/home")
-def home():
-    return render_template('home.html')
-    
-@application.route("/about")
-def about():
-    return render_template('about.html', title='About')
-    
-@application.route("/register", methods=['GET','POST'])
+@application.route("/register", methods=['POST'])
 def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
-	return render_template('register.html', title='Register', form=form)
+	return
 	
-@application.route("/login", methods=['GET','POST'])
+@application.route("/login", methods=['POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-			flash(f'You have been logged in!', 'success')
-			return redirect(url_for('home'))
-		else:
-			flash(f'Login Unsuccessful. Please check username and password', 'danger')
-	return render_template('login.html', title='Login', form=form)
+	return
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
