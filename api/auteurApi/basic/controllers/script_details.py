594967953d6abae36876b2c342489b9d495e9ce2
achansarkar <achansarkar@barracuda.com>
2019-05-07 14:17:07 -0700

    fixed script_details

--- api/auteurApi/basic/controllers/script_details.py
+++ api/auteurApi/basic/controllers/script_details.py
@@ -10,6 +10,10 @@ class Script_Details(Base):
     def __init__(self, request):
         super().__init__(request)
 
-    def get(self, script_unique_id):
-        r = {"dialog_ratio":50}
+    def get(self, in_url):
+        self.log.debug(in_url)
+        script_unique_id = in_url.strip('/details')
+        script_obj = Scripts()
+        data = script_obj.get_by_unique_id(script_unique_id)
+        r = {"dialog_ratio":40, "avg_scene_length": "100 lines", "pages":data['page_count']}
         return self.response(r)
